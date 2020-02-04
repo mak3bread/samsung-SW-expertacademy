122c758bcbf52836c4c1897299a3651e18426be0
while95 <54495632+while95@users.noreply.github.com>
2020-02-04 23:00:16 +0900

    Add files via upload

--- D2/samsung SW 1946 간단한 압축 풀기/samsung SW 1946 간단한 압축 풀기/main.cpp
+++ D2/samsung SW 1946 간단한 압축 풀기/samsung SW 1946 간단한 압축 풀기/main.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 using namespace std;
 
+struct docu{
+    int num=0;
+    string s;
+};
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -12,20 +16,29 @@ int main(){
     for(int test=1;test<=T;test++){
         int N;
         cin>>N;
+        int sum=0;
+        
+        vector<docu*> doc;
         for(int i=0;i<N;i++){
-            string s;
-            int Ki;
-            cin>>s>>Ki;
-            
-            int cnt=0;
-            while(1){
-                if(cnt==10){cout<<'\n';}
-                if(Ki==0){break;}
-                cout<<s;
-                cnt++;
-                Ki--;
-            }
+            docu* dc=new docu;
+            cin>>dc->s>>dc->num;
+            doc.push_back(dc);
+            sum+=dc->num;
         }
+        int i=0,j=0;
+        int cnt=0;
+        
+        cout<<"#"<<test<<'\n';
+        while(1){
+            cout<<doc[i]->s;
+            cnt++;
+            j++;
+            if(cnt%10==0&&cnt!=0){cout<<'\n';}
+            if(doc[i]->num==j){j=0;i++;}
+            if(sum==cnt){break;}
+        }
+        cout<<'\n';
+        doc.clear();
     }
     
     return 0;
